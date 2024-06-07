2f9359ec5cbea87a97806aefe4e6ca55c2185a9f
nicolasribeiroh <nicolasribeiroh@gmail.com>
2024-06-06 23:07:48 -0300

    funçoes data e hora completas, iniciado novas funções de ajuda, saida do programa e recap caso comando não seja executado de forma corrta, esperando por mais funções desejadas para que sejam aplicadas

--- TalkBot/Principal.java
+++ TalkBot/Principal.java
@@ -1,5 +1,4 @@
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
@@ -9,30 +8,61 @@ import java.util.Locale;
 public class Principal {
     public static void main(String[] args){
         
-        String nome;
-        String pergunta;
-        String[] tempo = new String[] {"Clima","Tempo","clima","tempo"};
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Digite seu nome: ");
-        nome = sc.nextLine();
+        System.out.print("Olá, tudo bem? qual seu nome? ");
+        String nome = sc.nextLine();
 
-        System.out.printf("Olá %s, Tudo Bem?",nome);
+        //Declaração para verificar as perguntas        
+        String hora = "hora";
+        String data = "data";
+        String despedida = "tchau";
+        String ajuda = "ajuda";
+
+       
+        System.out.printf("Seja bem vindo %s!!",nome);
         System.out.print("\nO que gostaria de perguntar? ");
-        pergunta = sc.nextLine();
-
-        boolean p1= pergunta.equals(tempo);
-        if (p1==true){
-            Hora();
-
-        }
-
+        String pergunta = sc.nextLine();
         
+        while (true){
+            
 
-        
+
+            //Ação caso a pergunta seja feita com a palavra chave 
+            if(pergunta.toLowerCase().contains(hora.toLowerCase())){
+                System.out.println(Hora());
+                System.out.print("Gostaria de fazer outra pergunta? caso contrario se despida: ");
+                pergunta =sc.nextLine();
+
+            }
+            if(pergunta.toLowerCase().contains(data.toLowerCase())){
+                System.out.println(Data());
+                System.out.print("Gostaria de fazer outra pergunta? caso contrario se despida: ");
+                pergunta =sc.nextLine();
+            }
+
+            if(pergunta.toLowerCase().contains(despedida.toLowerCase())){
+                break;
+            }
+
+            if (pergunta.toLowerCase().contains(ajuda.toLowerCase())){
+                System.out.println("Você pode fazer perguntas como \n-Data\n-Hora\n-Ajuda");
+                pergunta =sc.nextLine();
+
+            }
+            else{
+                System.out.print("Desculpe, não entendi sua pergunta poderia reformula-la:");
+                pergunta =sc.nextLine();
+
+            }
+      }
+
+        System.out.println("Tchau tchau, até uma próxima vez ;)");
+
     }
+    
 
-   //Classe Data
+    //Classe Data
     private static String Data(){
         LocalDate LocalDate=java.time.LocalDate.now();
         DateTimeFormatter localDateTimeFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy", new Locale("PT","br" ));
